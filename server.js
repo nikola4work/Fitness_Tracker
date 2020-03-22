61cddfb12c0ce4f9fb1e8ec35802aa012ddd6e73
nikola <nikola4work@gmail.com>
2020-03-22 15:29:04 -0400

    install express dependacies and created port for the app

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 2020;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var routes = require("./controller/fitness_controller");
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`)
+})
